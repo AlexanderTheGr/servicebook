728262fd26222451122d1eff9108bfc4db680a4e
alexanderthegr <a.dimeas@gmail.com>
2017-09-10 23:53:11 +0300

    *-

--- src/ServicebookBundle/Controller/ApiController.php
+++ src/ServicebookBundle/Controller/ApiController.php
@@ -8,10 +8,39 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Controller\Main as Main;
 
-class ApiController extends Controller
-{
-    public function indexAction($name)
-    {
+class ApiController extends Controller {
+
+    public function indexAction($name) {
         return $this->render('ServicebookBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    /**
+     * 
+     * 
+     * @Route("/api/getBrands")
+     */
+    public function getProducts(Request $request) {
+        //$allowedips = $this->getSetting("ServicebookBundle:Api:Allowedips");
+       // $allowedipsArr = explode(",", $allowedips);
+        //if (in_array($_SERVER["REMOTE_ADDR"], $allowedipsArr)) {
+            $sql = "SELECT * FROM  `service_brand` where enable = 1";
+            $connection = $this->getDoctrine()->getConnection();
+            $statement = $connection->prepare($sql);
+            $statement->execute();
+            $results = $statement->fetchAll();
+            $arr = array();
+            foreach ($results as $data) {
+                $arr[] = $data;
+            }
+
+            $data["status"] = "OK";
+            $data["data"] = $arr;
+            $json = json_encode($data);
+            return new Response(
+                    $json, 200, array('Content-Type' => 'application/json')
+            );
+        //} else {
+        //    exit;
+        //}
     }
 }
