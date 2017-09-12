c3104e97d563eb16f19355deb82db7ea6daf5b06
alexanderthegr <a.dimeas@gmail.com>
2017-09-12 20:22:25 +0300

    *-

--- src/ServicebookBundle/Controller/UserController.php
+++ src/ServicebookBundle/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace ServicebookBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Controller\Main as Main;
