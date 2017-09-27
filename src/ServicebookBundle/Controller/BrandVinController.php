cd51a24addf9c81017afab6c6b86bd9b84a4c102
alexanderthegr <a.dimeas@gmail.com>
2017-09-27 20:41:15 +0300

    *-

--- src/ServicebookBundle/Controller/BrandVinController.php
+++ src/ServicebookBundle/Controller/BrandVinController.php
@@ -83,6 +83,7 @@ class BrandVinController extends Main {
         }
 
         $fields["vin"] = array("label" => "Vin", 'required' => true);
+        $fields["productSale"] = array("label" => "Brand", "className" => "col-md-3", 'type' => "select", "required" => true, 'datasource' => array('repository' => 'SerivebookBundle:Brand', 'name' => 'brand', 'value' => 'id'));
         $fields["model"] = array("label" => "Model", 'required' => false);
         $fields["engine"] = array("label" => "Engine");
         $fields["displacement"] = array("label" => "Displacement");
