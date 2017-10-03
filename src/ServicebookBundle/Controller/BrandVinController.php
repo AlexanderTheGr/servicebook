7b5d1cc5850669d865413583768ecedf82368b4a
alexanderthegr <a.dimeas@gmail.com>
2017-10-03 23:06:39 +0300

    *-

--- src/ServicebookBundle/Controller/BrandVinController.php
+++ src/ServicebookBundle/Controller/BrandVinController.php
@@ -237,9 +237,9 @@ class BrandVinController extends Main {
             $params['view'] = '/servicebook/brandvin/servicepart/view';
             $params['viewnew'] = '/servicebook/brandvin/servicepart/view/new/' . $id;
 
-            $params['key'] = 'gettabspart_' . $id;
-            $params["ctrl"] = 'ctrlgettabparts';
-            $params["app"] = 'appgettabparts';
+            $params['key'] = 'gettabsp_' . $id;
+            $params["ctrl"] = 'ctrlgettabs';
+            $params["app"] = 'appgettabs';
             $datatables[] = $this->contentDatatable($params);
         }        
         
