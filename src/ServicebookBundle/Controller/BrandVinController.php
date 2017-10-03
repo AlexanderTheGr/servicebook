aadd46f39eafc226c34b828405023ef9b67de161
alexanderthegr <a.dimeas@gmail.com>
2017-10-03 21:44:07 +0300

    *-

--- src/ServicebookBundle/Controller/BrandVinController.php
+++ src/ServicebookBundle/Controller/BrandVinController.php
@@ -149,7 +149,7 @@ class BrandVinController extends Main {
             $params['dtparams'] = $dtparams;
             $params['id'] = $dtparams;
             $params['url'] = '/servicebook/brandvin/getservices/' . $id;
-            $params['view'] = '/servicebook/brandvin/service/view';
+            $params['view'] = '/servicebook/brandvin/service/view/new/'.$id;
             $params['key'] = 'gettabs_' . $id;
             $params["ctrl"] = 'ctrlgettabs';
             $params["app"] = 'appgettabs';
