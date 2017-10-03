85917ecb0a37b580ea9cd3fea222d90ec3ec661a
alexanderthegr <a.dimeas@gmail.com>
2017-10-03 23:46:50 +0300

    *-

--- src/ServicebookBundle/Controller/BrandVinController.php
+++ src/ServicebookBundle/Controller/BrandVinController.php
@@ -322,6 +322,7 @@ class BrandVinController extends Main {
         if ($id > 0 AND count($entity) > 0) {
             $dtparams[] = array("name" => "ID", "index" => 'id', "active" => "active");
             $dtparams[] = array("name" => "Title", "index" => 'part');
+            $dtparams[] = array("name" => "Brand", "index" => 'brand');
             $dtparams[] = array("name" => "Code", "index" => 'code');
             //$dtparams[] = array("name" => "Price", "index" => 'storeWholeSalePrice');
             $params['dtparams'] = $dtparams;
