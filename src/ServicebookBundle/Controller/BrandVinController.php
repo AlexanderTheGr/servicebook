fa095c47e8d353a9187528a15b982ceb47f16f10
alexanderthegr <a.dimeas@gmail.com>
2017-10-04 21:02:30 +0300

    *-

--- src/ServicebookBundle/Controller/BrandVinController.php
+++ src/ServicebookBundle/Controller/BrandVinController.php
@@ -57,7 +57,7 @@ class BrandVinController extends Main {
 
         $jsonarr = array();
         if ($entity->getId()) {
-            $jsonarr["returnurl"] = "/servicebook/brandvin/service/view/" . $entity->getBrandService()->getId();
+            $jsonarr["returnurl"] = "/servicebook/brandvin/service/view/" . $entity->getBrandServiceAction()->getId();
         }
 
         $json = json_encode($jsonarr);
