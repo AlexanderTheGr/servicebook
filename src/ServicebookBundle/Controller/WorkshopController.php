78a94921c51ad4a0f6869f095d000307a6b8ba15
alexanderthegr <a.dimeas@gmail.com>
2017-11-20 23:06:21 +0200

    *-

--- src/ServicebookBundle/Controller/WorkshopController.php
+++ src/ServicebookBundle/Controller/WorkshopController.php
@@ -3,9 +3,12 @@
 namespace ServicebookBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Controller\Main as Main;
+use ServicebookBundle\Entity\Workshop as Workshop;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class WorkshopController extends Main {
 
