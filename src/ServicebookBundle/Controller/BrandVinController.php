498e1f4527bbe7db113ba4181ee7034d5fdd6160
alexanderthegr <a.dimeas@gmail.com>
2017-11-20 21:17:50 +0200

    *-

--- src/ServicebookBundle/Controller/BrandVinController.php
+++ src/ServicebookBundle/Controller/BrandVinController.php
@@ -224,47 +224,7 @@ class BrandVinController extends Main {
         ));
     }
 
-    /**
-     * @Route("/servicebook/brandvin/service/view/{id}/{vin}")
-     */
-    public function serviceAction($id, $vin = false) {
-        $this->repository = "ServicebookBundle:BrandService";
-        $buttons = array();
-        $content = $this->getservicetabs($id);
-        //$content = $this->getoffcanvases($id);
-        $pagename = "Service";
-        if ($id > 0) {
-            $entity = $this->getDoctrine()
-                    ->getRepository($this->repository)
-                    ->find($id);
-            $vin = $entity->getBrandVin()->getId();
 
-            $pagename = "Service: (" . $entity->getService() . ")";
-            $vinpagenane = "Vin: (" . $entity->getBrandVin()->getVin() . ")";
-            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/brandvin/view/' . $vin . '">' . $vinpagenane . '</a>';
-            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/brandvin/service/view/' . $id . '/' . $vin . '">' . $pagename . '</a>';
-        } else {
-
-            $entity = $this->getDoctrine()
-                    ->getRepository('ServicebookBundle:BrandVin')
-                    ->find($vin);
-            $vinpagenane = "Vin: (" . $entity->getVin() . ")";
-            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/brandvin/view/' . $vin . '">' . $vinpagenane . '</a>';
-            $breadcrumb[] = 'New Service';
-            $pagename = 'New Service';
-        }
-        $content = $this->content();
-        return $this->render('ServicebookBundle:BrandVin:view.html.twig', array(
-                    'pagename' => $pagename,
-                    'breadcrumb' => implode(" / ", $breadcrumb),
-                    'url' => '/servicebook/brandvin/service/save/' . $vin,
-                    'buttons' => $buttons,
-                    'ctrl' => $this->generateRandomString(),
-                    'app' => $this->generateRandomString(),
-                    'content' => $content,
-                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-        ));
-    }
 
     /**
      * @Route("/servicebook/brandvin/serviceaction/view/{id}/{service}")
@@ -674,33 +634,6 @@ class BrandVinController extends Main {
         );
     }
 
-    /**
-     * @Route("/servicebook/brandvin/getservices/{id}")
-     */
-    public function getservicesAction($id) {
-        $session = new Session();
-        foreach ($session->get('params_gettabs_' . $id) as $param) {
-            $this->addField($param);
-        }
-        $this->repository = 'ServicebookBundle:BrandService';
-        $this->q_and[] = $this->prefix . ".brandVin = '" . $id . "'";
-        $json = $this->datatable();
-
-        $datatable = json_decode($json);
-        $datatable->data = (array) $datatable->data;
-        foreach ($datatable->data as $key => $table) {
-            $table = (array) $table;
-            $table1 = array();
-            foreach ($table as $f => $val) {
-                $table1[$f] = $val;
-            }
-            $datatable->data[$key] = $table1;
-        }
-        $json = json_encode($datatable);
-        return new Response(
-                $json, 200, array('Content-Type' => 'application/json')
-        );
-    }
 
     /**
      * @Route("/servicebook/brandvin/getkm/{id}")
