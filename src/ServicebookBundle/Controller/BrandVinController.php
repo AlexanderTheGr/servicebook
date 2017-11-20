ce4f14e9364112bc15efac6be39307e9861c4d4c
alexanderthegr <a.dimeas@gmail.com>
2017-11-20 21:12:04 +0200

    *-

--- src/ServicebookBundle/Controller/BrandVinController.php
+++ src/ServicebookBundle/Controller/BrandVinController.php
@@ -101,41 +101,6 @@ class BrandVinController extends Main {
         );
     }
 
-    /**
-     * @Route("/servicebook/brandvin/service/save/{vin}")
-     */
-    public function servicesaveAction($vin = false) {
-        $this->repository = "ServicebookBundle:BrandService";
-        $dt = new \DateTime("now");
-        $entity = new \ServicebookBundle\Entity\BrandService;
-        $this->newentity[$this->repository] = $entity;
-        $this->initialazeNewEntity($entity);
-        $this->newentity[$this->repository]->setField("status", 1);
-        $brandVin = $this->getDoctrine()
-                ->getRepository("ServicebookBundle:BrandVin")
-                ->find($vin);
-        $this->newentity[$this->repository]->setField("brandVin", $brandVin);
-
-        $entities = $this->save();
-
-        $entity = $this->getDoctrine()
-                ->getRepository($this->repository)
-                ->find($entities[$this->repository]);
-
-        $entity->setTs($dt);
-        $entity->setModified($dt);
-        $this->flushpersist($entity);
-
-        $jsonarr = array();
-        if ($entity->getId()) {
-            $jsonarr["returnurl"] = "/servicebook/brandvin/view/" . $entity->getBrandVin()->getId();
-        }
-
-        $json = json_encode($jsonarr);
-        return new Response(
-                $json, 200, array('Content-Type' => 'application/json')
-        );
-    }
 
     /**
      * @Route("/servicebook/brandvin/km/save/{vin}")
@@ -784,5 +749,4 @@ class BrandVinController extends Main {
                 $json, 200, array('Content-Type' => 'application/json')
         );
     }
-
 }
