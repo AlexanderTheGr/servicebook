fc9b0e6b9f0f0fd099f258baf8f2ef9bc339880a
alexanderthegr <a.dimeas@gmail.com>
2017-11-22 22:28:51 +0200

    *-

---
+++ src/ServicebookBundle/Controller/WorkshopServiceActionPartController.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace ServicebookBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Controller\Main as Main;
+use ServicebookBundle\Entity\BrandVin as BrandVin;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+class WorkshopServiceActionPartController extends Main {
+
+    var $repository = 'ServicebookBundle:WorkshopServicePart';
+    /**
+     * @Route("/servicebook/workshop/servicepart/save/{action}")
+     */
+    public function saveAction($action = false) {
+        $this->repository = "ServicebookBundle:WorkshopServicePart";
+        $dt = new \DateTime("now");
+        $entity = new \ServicebookBundle\Entity\WorkshopServicePart;
+        $this->newentity[$this->repository] = $entity;
+        $this->initialazeNewEntity($entity);
+        $workshopServiceAction = $this->getDoctrine()
+                ->getRepository("ServicebookBundle:WorkshopServiceAction")
+                ->find($action);
+
+        $this->newentity[$this->repository]->setField("workshopServiceAction", $workshopServiceAction);
+
+        $entities = $this->save();
+
+        $entity = $this->getDoctrine()
+                ->getRepository($this->repository)
+                ->find($entities[$this->repository]);
+
+        $entity->setTs($dt);
+        $entity->setModified($dt);
+        $this->flushpersist($entity);
+
+        $jsonarr = array();
+        if ($entity->getId()) {
+            $jsonarr["returnurl"] = "/servicebook/workshop/serviceaction/view/" . $entity->getWorkshopServiceAction()->getId();
+        }
+
+        $json = json_encode($jsonarr);
+        return new Response(
+                $json, 200, array('Content-Type' => 'application/json')
+        );
+    }
+
+    /**
+     * @Route("/servicebook/workshop/servicepart/view/{id}/{action}")
+     */
+    public function viewAction($id, $action = false) {
+        $this->repository = "ServicebookBundle:WorkshopServicePart";
+        $buttons = array();
+        $content = $this->gettabs($id);
+        //$content = $this->getoffcanvases($id);
+        $pagename = "Part";
+        $breadcrumb = array();
+
+        if ($id > 0) {
+            $entity = $this->getDoctrine()
+                    ->getRepository($this->repository)
+                    ->find($id);
+            $action = $entity->getWorkshopServiceAction()->getId();
+            $pagename = "Part (" . $entity->getBrand()->getBrand() . "  " . $entity->getPart() . " " . $entity->getCode() . ")";
+
+            //$pagename = 'Action: (' . $entity->getAction() . ')';
+            $vinpagenane = "Vin (" . $entity->getWorkshopServiceAction()->getWorkshopService()->getWorkshop()->getName() . ")";
+            $servicepagenane = "Service: (" . $entity->getWorkshopServiceAction()->getWorkshopService()->getService() . ")";
+            $serviceactionpagename = 'Action: (' . $entity->getWorkshopServiceAction()->getAction() . ')';
+            $breadcrumb = array();
+            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/workshop/view/' . $entity->getWorkshopServiceAction()->getWorkshopService()->getWorkshop()->getId() . '">' . $vinpagenane . '</a>';
+            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/workshop/service/view/' . $entity->getWorkshopServiceAction()->getWorkshopService()->getId() . '/' . $entity->getWorkshopServiceAction()->getWorkshopService()->getWorkshop()->getId() . '">' . $servicepagenane . '</a>';
+            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/workshop/serviceaction/view/' . $entity->getWorkshopServiceAction()->getId() . '/' . $entity->getWorkshopServiceAction()->getWorkshopService()->getId() . '">' . $serviceactionpagename . '</a>';
+            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/workshop/servicepart/view/' . $id . '/' . $action . '">' . $pagename . '</a>';
+        } else {
+            $entity = $this->getDoctrine()
+                    ->getRepository("ServicebookBundle:WorkshopServiceAction")
+                    ->find($action);
+            $service = $entity->getWorkshopService()->getId();
+
+            $serviceactionpagename = 'Action: (' . $entity->getAction() . ')';
+            $vinpagenane = "Vin (" . $entity->getWorkshopService()->getWorkshop()->getName() . ")";
+            $servicepagenane = "Service: (" . $entity->getWorkshopService()->getService() . ")";
+            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/workshop/view/' . $entity->getWorkshopService()->getWorkshop()->getId() . '">' . $vinpagenane . '</a>';
+            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/workshop/service/view/' . $entity->getWorkshopService()->getId() . '/' . $entity->getWorkshopService()->getWorkshop()->getId() . '">' . $servicepagenane . '</a>';
+            $breadcrumb[] = '<a class="breadcrumb" href="/servicebook/workshop/serviceaction/view/' . $action . '/' . $service . '">' . $serviceactionpagename . '</a>';
+            $breadcrumb[] = "New Part";
+            $pagename = 'New Part';
+        }
+        $content = $this->content();
+        return $this->render('ServicebookBundle:BrandVin:view.html.twig', array(
+                    'pagename' => $pagename,
+                    'breadcrumb' => implode(" / ", $breadcrumb),
+                    'url' => '/servicebook/workshop/servicepart/save/' . $action,
+                    'buttons' => $buttons,
+                    'ctrl' => $this->generateRandomString(),
+                    'app' => $this->generateRandomString(),
+                    'content' => $content,
+                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
+        ));
+    }
+
+    public function gettabs($id) {
+        $this->repository = "ServicebookBundle:WorkshopServicePart";
+        $entity = $this->getDoctrine()
+                ->getRepository("ServicebookBundle:WorkshopServicePart")
+                ->find($id);
+
+        if ($id == 0 AND @ $entity->id == 0) {
+            $entity = new \ServicebookBundle\Entity\WorkshopServicePart;
+            $this->newentity[$this->repository] = $entity;
+        }
+        $dataarray[] = array("value" => "0", "name" => "Oxi");
+        $dataarray[] = array("value" => "1", "name" => "Ναι");
+
+        $fields["part"] = array("label" => "Part", 'required' => true);
+        //$fields["brand"] = array("label" => "Brand", 'required' => true);
+        $fields["brand"] = array("label" => "Brand", "disabled" => false, 'type' => "select", "required" => true, 'datasource' => array('repository' => 'ServicebookBundle:Brand', 'name' => 'brand', 'value' => 'id'));
+
+        $fields["code"] = array("label" => "Code", 'required' => true);
+        //$fields["details"] = array("label" => "Details", "type" => "textarea");
+        //$fields["brand"] = array("label" => "Brand", "disabled" => true, "className" => "col-md-6", 'type' => "select", "required" => true, 'datasource' => array('repository' => 'ServicebookBundle:Brand', 'name' => 'brand', 'value' => 'id'));
+        //$fields["user"] = array("label" => "User", "disabled" => true, "className" => "col-md-6", 'type' => "select", "required" => true, 'datasource' => array('repository' => 'ServicebookBundle:User', 'name' => 'name', 'value' => 'id'));
+        //$fields["brandVin:id"] = array("label" => "Name");
+        //$fields["brandVin"] = array("label" => "Brand Vin", "disabled" => true, "className" => "col-md-6", 'type' => "select", "required" => true, 'datasource' => array('repository' => 'ServicebookBundle:BrandVin', 'name' => 'vin', 'value' => 'id'));
+
+        $forms = $this->getFormLyFields($entity, $fields);
+
+        $this->addTab(array("title" => "General", "form" => $forms, "content" => '', "index" => $this->generateRandomString(), 'search' => 'text', "active" => true));
+
+        $json = $this->tabs();
+        //echo json_encode($json);
+        return $json;
+    }
+
+    /**
+     * @Route("/servicebook/workshop/getserviceparts/{id}")
+     */
+    public function getservicepartsAction($id) {
+        $session = new Session();
+        foreach ($session->get('params_gettabs_' . $id) as $param) {
+            $this->addField($param);
+        }
+        $this->repository = 'ServicebookBundle:WorkshopServicePart';
+        $this->q_and[] = $this->prefix . ".workshopServiceAction = '" . $id . "'";
+        $json = $this->datatable();
+
+        $datatable = json_decode($json);
+        $datatable->data = (array) $datatable->data;
+        foreach ((array) $datatable->data as $key => $table) {
+            $table = (array) $table;
+            $table1 = array();
+            foreach ($table as $f => $val) {
+                $table1[$f] = $val;
+            }
+            $datatable->data[$key] = $table1;
+        }
+        $json = json_encode($datatable);
+        return new Response(
+                $json, 200, array('Content-Type' => 'application/json')
+        );
+    }
+
+}
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
