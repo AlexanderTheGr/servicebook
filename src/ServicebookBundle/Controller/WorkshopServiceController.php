9da255555fef7640219f792fca7d2a7758d43119
alexanderthegr <a.dimeas@gmail.com>
2017-11-22 20:36:01 +0200

    *-

--- src/ServicebookBundle/Controller/WorkshopServiceController.php
+++ src/ServicebookBundle/Controller/WorkshopServiceController.php
@@ -87,9 +87,9 @@ class WorkshopServiceController extends Main {
 
         echo count($service->getActions()) . ",";
         foreach ($service->getActions() as $action) {
-            echo count($action->getParts() . ",");
+            echo "[".count($action->getParts() . "],");
             foreach ($action->getParts() as $brandServicePart) {
-                echo "[".$brandServicePart->getId()."]";
+                //echo "[".$brandServicePart->getId()."]";
             }
         }
         $entity = $this->getDoctrine()
